a27c9c0649b523cd199f31b18941b4d16fe3ad4f
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-23 18:44:24 -0300

    Feat: Created Post component

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Header from './src/components/Header';
+import Post from './src/components/Post';
 
 export default class App extends Component {
   Render() {
-    <Header />;
+    <View>
+      <Header />;
+      <Post image={require('./assets/imgs/fence.jpg')} />
+    </View>;
   }
 }
