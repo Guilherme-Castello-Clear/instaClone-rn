e4c2df5d845f35e5b48907e42d01521bced1a545
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-24 12:58:29 -0300

    Feat: Using Firebase 1/3

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,14 @@
 
 const {onRequest} = require('firebase-functions/v2/https');
 const logger = require('firebase-functions/logger');
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-exports.helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', {structuredData: true});
-  response.send('Hello from Firebase!');
+const cors = require('cors')({origin: true});
+const fs = require('fs');
+const uuid = require('uuid-v4');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage({
+  projectId: 'instaclone-1cd6e',
+  keyFilename: 'instaClone.json',
+});
+exports.uploadImage = onRequest((request, response) => {
+  cors(request, response, () => {});
 });
