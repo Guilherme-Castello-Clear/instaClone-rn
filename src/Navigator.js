e5476448a3d60cfb497045a1cb7611b922289cc4
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-24 15:30:41 -0300

    Feat: Added Splash

--- src/Navigator.js
+++ src/Navigator.js
@@ -10,7 +10,7 @@ import AddPhoto from './screens/AddPhoto';
 import Profile from './screens/Profile';
 import Login from './screens/Login';
 import Register from './screens/Register';
-
+import Splash from './screens/Splash';
 const authRouter = createStackNavigator(
   {
     Login: {screen: Login, navigationOptions: {title: 'Login'}},
@@ -72,4 +72,12 @@ const MenuConfig = {
 };
 
 const MenuNavigator = createBottomTabNavigator(MenuRoutes, MenuConfig);
-export default MenuNavigator;
+const SplashRouter = createSwitchNavigator(
+  {
+    Splash: Splash,
+    App: MenuNavigator,
+  },
+  {initialRouteName: 'Splash'},
+);
+
+export default SplashRouter;
