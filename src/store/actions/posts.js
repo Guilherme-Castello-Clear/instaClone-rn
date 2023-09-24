189fc36f2a0abf38b7debb7d53e1f57ea2a3e3e5
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-24 13:24:58 -0300

    Feat: Using Firebase 3/3

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -2,11 +2,25 @@ import {ADD_POST, ADD_COMMENT} from './actionTypes';
 import axios from 'axios';
 export const addPost = post => {
   return dispatch => {
-    axios
-      .post('/posts.json', {...post})
-      .catch(err => console.log(err))
-      .then(res => {
-        console.log(res);
+    axios({
+      url: 'uploadImage',
+      baseURL: 'https://us-central1-instaclone-1cd6e.cloudfunctions.net',
+      method: 'post',
+      data: {
+        image: post.image.base64,
+      },
+    })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(resp => {
+        post.image = resp.data.imageUrl;
+        axios
+          .post('/posts.json', {...post})
+          .catch(err => console.log(err))
+          .then(res => {
+            console.log(res);
+          });
       });
   };
 };
