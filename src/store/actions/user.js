caf0b0473da496dd461c20be42d3263116c41bb2
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-24 14:46:45 -0300

    Feat: Creating user with firebase authentication (Email)

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,5 +1,9 @@
 import {USER_LOGGED_IN, USER_LOGGED_OUT} from './actionTypes';
-
+import axios from 'axios';
+const authBaseURL =
+  'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
+const API_KEY =
+  'luBok4fVNUyYTUHikWOnmXe8OjeEjnMBnCzMeRRkCJaG0Mzfjq6Nd1O1741CZ9foNj9oibMfMzVS6hTBIf2TWw==';
 export const login = user => {
   return {
     type: USER_LOGGED_IN,
@@ -10,5 +14,33 @@ export const login = user => {
 export const logout = () => {
   return {
     type: USER_LOGGED_OUT,
+  };
+};
+
+export const createUser = user => {
+  return dispatch => {
+    axios
+      .post(`${authBaseURL}/signupNewUser?key=${API_KEY}`, {
+        email: user.email,
+        password: user.password,
+        returnSecureToken: true,
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(res => {
+        if (res.data.localId) {
+          axios
+            .put(`/users/${res.data.localId}.json`, {
+              name: user.name,
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            .then(() => {
+              console.log('Cadastrado');
+            });
+        }
+      });
   };
 };
